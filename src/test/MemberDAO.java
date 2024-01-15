6a0e2358597e3b2900f39158633bef2b86d96c7c
JeongHoonSeok <wjdgnstjr93@gmail.com>
2024-01-15 14:40:39 +0900

    회원 기능 수정

--- src/test/MemberDAO.java
+++ src/test/MemberDAO.java
@@ -39,8 +39,8 @@ public class MemberDAO {
 
 	// 회원가입
 	private static final String INSERT = "INSERT INTO "
-			+ "MEMBER (M_ID, M_NAME, M_PASSWORD, DOB, GENDER, PHONE_NUMBER, EMAIL, ADDRESS, GRADE, HEALTH) "
-			+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, 'USER', ?)";
+			+ "MEMBER (M_ID, M_NAME, M_PASSWORD, DOB, GENDER, PHONE_NUMBER, EMAIL, POSTCODE, ADDRESS, DETAILED_ADDRESS, GRADE, HEALTH) "
+			+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 'USER', ?)";
 
 	private static final String UPDATE = "";
 
@@ -143,8 +143,10 @@ public class MemberDAO {
 				pstmt.setString(5, mDTO.getGender());
 				pstmt.setString(6, mDTO.getPhoneNumber());
 				pstmt.setString(7, mDTO.getEmail());
-				pstmt.setString(8, mDTO.getAddress());
-				pstmt.setString(9, mDTO.getHealth());
+				pstmt.setInt(8, mDTO.getPostCode());
+				pstmt.setString(9, mDTO.getAddress());
+				pstmt.setString(10, mDTO.getDetailedAddress());
+				pstmt.setString(11, mDTO.getHealth());
 				int rs = pstmt.executeUpdate();
 				if (rs <= 0) {
 					return false;
